deefd0076c1d3f2d87d5974f6cb8b70404582d6d
Nguyen Trung Thien <269.tdd@gmail.com>
2022-09-19 17:27:29 +0700

    create new project done

---
+++ QLCGV/Login.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace QLCGV
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textPass = new System.Windows.Forms.TextBox();
+            this.textTk = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(295, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Đăng nhập";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textPass
+            // 
+            this.textPass.Location = new System.Drawing.Point(281, 154);
+            this.textPass.Name = "textPass";
+            this.textPass.Size = new System.Drawing.Size(100, 20);
+            this.textPass.TabIndex = 1;
+            // 
+            // textTk
+            // 
+            this.textTk.Location = new System.Drawing.Point(281, 91);
+            this.textTk.Name = "textTk";
+            this.textTk.Size = new System.Drawing.Size(100, 20);
+            this.textTk.TabIndex = 2;
+            this.textTk.TextChanged += new System.EventHandler(this.textTk_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(272, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Login";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(517, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Đăng ký";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textTk);
+            this.Controls.Add(this.textPass);
+            this.Controls.Add(this.button1);
+            this.Name = "Login";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textPass;
+        private System.Windows.Forms.TextBox textTk;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ QLCGV/Login.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace QLCGV
+{
+    public partial class Login : Form
+    {
+        string tk = "admin";
+        string mk = "admin";
+        string tkuser = "user";
+        string mkuser = "user";
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textTk.Text == tk && textPass.Text == mk)
+            {
+                this.Hide();
+
+                var admin = new Admin.Admin();
+                admin.Show();
+
+            }
+            if (textTk.Text == tkuser && textPass.Text == mkuser)
+            {
+                this.Hide();
+                var user = new User.User();
+                user.Show();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            new DangKy().Show();
+        }
+
+        private void textTk_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
