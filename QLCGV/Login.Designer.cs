a0298d767fb7b89750c6d9cbc3f0fc05ae7f1ca8
PhanTrungTABD30822 <112456061+PhanTrungTABD30822@users.noreply.github.com>
2022-09-28 21:27:16 +0700

    update

--- QLCGV/Login.Designer.cs
+++ QLCGV/Login.Designer.cs
@@ -37,45 +37,55 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(295, 215);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(103, 202);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(120, 46);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Đăng nhập";
+            this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textPass
             // 
-            this.textPass.Location = new System.Drawing.Point(281, 154);
+            this.textPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textPass.Location = new System.Drawing.Point(103, 105);
+            this.textPass.Multiline = true;
             this.textPass.Name = "textPass";
-            this.textPass.Size = new System.Drawing.Size(100, 20);
+            this.textPass.Size = new System.Drawing.Size(235, 36);
             this.textPass.TabIndex = 1;
+            this.textPass.Text = "ID";
             // 
             // textTk
             // 
-            this.textTk.Location = new System.Drawing.Point(281, 91);
+            this.textTk.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textTk.Location = new System.Drawing.Point(103, 147);
+            this.textTk.Multiline = true;
             this.textTk.Name = "textTk";
-            this.textTk.Size = new System.Drawing.Size(100, 20);
+            this.textTk.Size = new System.Drawing.Size(235, 36);
             this.textTk.TabIndex = 2;
+            this.textTk.Text = "Password";
             this.textTk.TextChanged += new System.EventHandler(this.textTk_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(272, 35);
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(174, 50);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.Size = new System.Drawing.Size(102, 38);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Login";
+            this.label1.Text = "Log-in";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(517, 80);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(229, 202);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(109, 46);
             this.button2.TabIndex = 4;
-            this.button2.Text = "Đăng ký";
+            this.button2.Text = "Register";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -83,12 +93,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClientSize = new System.Drawing.Size(454, 365);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textTk);
             this.Controls.Add(this.textPass);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Login";
             this.Text = "Form1";
             this.ResumeLayout(false);
