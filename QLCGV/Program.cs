e89a48bd6ace0378b478177466d2c860a1ecbe37
NguyenAnhDuc2510 <nguyenanhduc2510@gmail.com>
2022-10-01 15:27:30 +0700

    bo may xong r
    123

--- QLCGV/Program.cs
+++ QLCGV/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QLCGV.Admin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace QLCGV
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Phong());
         }
     }
 }
