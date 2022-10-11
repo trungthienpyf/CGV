264aa8312a7e4c92d6e255192ce5669e4eddd72f
Hieu Nghia <lehieunghia2402@gmail.com>
2022-10-11 17:10:56 +0700

    Revert "Seccond-commit"
    This reverts commit 40064edaffcbf390b6777aad9f56dcd4454755e2.

--- QLCGV/Admin/KhachHang.cs
+++ QLCGV/Admin/KhachHang.cs
@@ -1,127 +1,20 @@
-﻿using BAL;
-using DAL;
-using DTO;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Net;
-using System.Reflection.Emit;
-using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace QLCGV.Admin
 {
     public partial class KhachHang : Form
     {
-        KhachHangBAL khachHangBAl = new KhachHangBAL();
         public KhachHang()
         {
             InitializeComponent();
-        }
-        private void btnSua_Click(object sender, EventArgs e)
-        {
-            KhachHangDTO khachHangDTO = new KhachHangDTO();
-            try
-            {
-                if(txtID.Text == "" || txtTenKH.Text == "" || txtDiaChi.Text == "" || txtSDT.Text == "" || txtMatKhau.Text == "" || txtEmail.Text == "")
-                {
-                    MessageBox.Show("Vui lòng điền đầy đủ thông tin cá nhân", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                }else
-                {
-                    khachHangDTO.tenKhachHang = txtTenKH.Text;
-                    khachHangDTO.ID = int.Parse(txtID.Text);
-                    khachHangDTO.sdt = txtSDT.Text;
-                    khachHangDTO.email = txtEmail.Text;
-                    khachHangDTO.matKhau = txtMatKhau.Text;
-                    khachHangDTO.diaChi = txtDiaChi.Text;
-                    string query = string.Format("id={0}&tenKhachhang={1}&SDT={2}&email={3}&matKhau={4}",
-                    khachHangDTO.ID, khachHangDTO.tenKhachHang, khachHangDTO.sdt, khachHangDTO.email, khachHangDTO.matKhau);
-                    bool checkStatus = khachHangBAl.updateData(query, int.Parse(txtID.Text));
-                    if(checkStatus == true)
-                    {
-                        MessageBox.Show("Cập nhật khách hàng thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                        Refesh();
-                    }else
-                    {
-                        MessageBox.Show("Cập nhật thất bại", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    }
-                }
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void btnXoa_Click(object sender, EventArgs e)
-        {
-            WebRequest request = WebRequest.Create("https://mfw060.wcom.vn/api/khachHang/" + txtID.Text);
-            request.Method = "DELETE";
-
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            if (response.StatusCode == HttpStatusCode.OK)
-                MessageBox.Show("Xóa thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            else
-                MessageBox.Show("Có lỗi xảy ra !!!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            ReLoad();
-            Refesh();
-        }
-       
-        private void Refesh()
-        {
-            txtID.Text = "";
-            txtTenKH.Text = "";
-            txtEmail.Text = "";
-            txtMatKhau.Text = "";
-            txtDiaChi.Text = "";
-            txtSDT.Text = "";
-        }
-
-
-        private void ReLoad()
-        {
-            List<KhachHangDTO> list = khachHangBAl.readData();
-
-            var LITS = new BindingList<KhachHangDTO>(list);
-            var source = new BindingSource(list, null);
-            dgvKhachHang.DataSource = source;
-            dgvKhachHang.Columns[0].HeaderText = "ID";
-            dgvKhachHang.Columns[1].HeaderText = "Tên Khách hàng";
-            dgvKhachHang.Columns[2].HeaderText = "SĐT";
-            dgvKhachHang.Columns[3].HeaderText = "Email";
-            dgvKhachHang.Columns[4].HeaderText = "Mật khẩu";
-            dgvKhachHang.Columns[5].HeaderText = "Địa chỉ";
-            
-        }
-
-        private void KhachHang_Load(object sender, EventArgs e)
-        {
-            ReLoad();
-        }
-
-        private void dgvKhachHang_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-
-        private void dgvKhachHang_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            dgvKhachHang.ReadOnly = true;
-            int i;
-            i = dgvKhachHang.CurrentRow.Index;
-            txtID.Text = dgvKhachHang.Rows[i].Cells[0].Value.ToString();
-            txtTenKH.Text = dgvKhachHang.Rows[i].Cells[1].Value.ToString();
-            txtSDT.Text = dgvKhachHang.Rows[i].Cells[2].Value.ToString();
-            txtEmail.Text = dgvKhachHang.Rows[i].Cells[3].Value.ToString();
-            txtMatKhau.Text = dgvKhachHang.Rows[i].Cells[4].Value.ToString();
-            txtDiaChi.Text = dgvKhachHang.Rows[i].Cells[5].Value.ToString();
         }
     }
 }
--- QLCGV/User/ChonGhe.Designer.cs
+++ QLCGV/User/ChonGhe.Designer.cs
@@ -28,219 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbTieuDe = new System.Windows.Forms.Label();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.pnGhe = new System.Windows.Forms.TableLayoutPanel();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.btnChon = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.btnThoat = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // lbTieuDe
-            // 
-            this.lbTieuDe.AutoSize = true;
-            this.lbTieuDe.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.89565F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbTieuDe.Location = new System.Drawing.Point(462, 48);
-            this.lbTieuDe.Name = "lbTieuDe";
-            this.lbTieuDe.Size = new System.Drawing.Size(117, 24);
-            this.lbTieuDe.TabIndex = 2;
-            this.lbTieuDe.Text = "Màn Chiếu ";
-            // 
-            // button4
-            // 
-            this.button4.BackColor = System.Drawing.Color.ForestGreen;
-            this.button4.Enabled = false;
-            this.button4.Location = new System.Drawing.Point(15, 17);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(30, 30);
-            this.button4.TabIndex = 8;
-            this.button4.UseVisualStyleBackColor = false;
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Red;
-            this.button1.Enabled = false;
-            this.button1.Location = new System.Drawing.Point(15, 69);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(30, 30);
-            this.button1.TabIndex = 9;
-            this.button1.UseVisualStyleBackColor = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.89565F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(51, 17);
+            this.label1.Location = new System.Drawing.Point(296, 63);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(103, 24);
-            this.label1.TabIndex = 10;
-            this.label1.Text = "Đang chọn";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chon ghe";
             // 
-            // label2
+            // button1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.89565F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(52, 69);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 24);
-            this.label2.TabIndex = 11;
-            this.label2.Text = "Đã mua";
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.button4);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(933, 395);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(196, 102);
-            this.panel1.TabIndex = 12;
-            // 
-            // pnGhe
-            // 
-            this.pnGhe.ColumnCount = 10;
-            this.pnGhe.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.pnGhe.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.pnGhe.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.pnGhe.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.pnGhe.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.pnGhe.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.pnGhe.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.pnGhe.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.pnGhe.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.pnGhe.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.pnGhe.Location = new System.Drawing.Point(35, 31);
-            this.pnGhe.Name = "pnGhe";
-            this.pnGhe.RowCount = 9;
-            this.pnGhe.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
-            this.pnGhe.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
-            this.pnGhe.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
-            this.pnGhe.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
-            this.pnGhe.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
-            this.pnGhe.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
-            this.pnGhe.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
-            this.pnGhe.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
-            this.pnGhe.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
-            this.pnGhe.Size = new System.Drawing.Size(780, 362);
-            this.pnGhe.TabIndex = 13;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.pnGhe);
-            this.groupBox1.Location = new System.Drawing.Point(97, 88);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(830, 422);
-            this.groupBox1.TabIndex = 14;
-            this.groupBox1.TabStop = false;
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.groupBox2.Controls.Add(this.btnThoat);
-            this.groupBox2.Controls.Add(this.button2);
-            this.groupBox2.Controls.Add(this.btnChon);
-            this.groupBox2.Controls.Add(this.textBox1);
-            this.groupBox2.Controls.Add(this.label3);
-            this.groupBox2.Location = new System.Drawing.Point(12, 534);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(1170, 130);
-            this.groupBox2.TabIndex = 15;
-            this.groupBox2.TabStop = false;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(907, 35);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(183, 22);
-            this.textBox1.TabIndex = 1;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(813, 38);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(81, 16);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Thành Tiền: ";
-            // 
-            // btnChon
-            // 
-            this.btnChon.Location = new System.Drawing.Point(101, 79);
-            this.btnChon.Name = "btnChon";
-            this.btnChon.Size = new System.Drawing.Size(75, 23);
-            this.btnChon.TabIndex = 16;
-            this.btnChon.Text = "Chọn";
-            this.btnChon.UseVisualStyleBackColor = true;
-            this.btnChon.Click += new System.EventHandler(this.btnChon_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(248, 79);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 17;
-            this.button2.Text = "Hủy";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // btnThoat
-            // 
-            this.btnThoat.Location = new System.Drawing.Point(425, 79);
-            this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(75, 23);
-            this.btnThoat.TabIndex = 18;
-            this.btnThoat.Text = "Thoát";
-            this.btnThoat.UseVisualStyleBackColor = true;
-            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            this.button1.Location = new System.Drawing.Point(339, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Thanh toan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // ChonGhe
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1194, 666);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.lbTieuDe);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.Name = "ChonGhe";
             this.Text = "ChonGhe";
-            this.Load += new System.EventHandler(this.ChonGhe_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Label lbTieuDe;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button1;
+
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TableLayoutPanel pnGhe;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button btnChon;
-        private System.Windows.Forms.Button btnThoat;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
